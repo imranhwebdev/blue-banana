9da9e31fadb1b3de2035df15bbd1b6ac14505241
imran <imranh.coder@gmail.com>
2024-12-07 14:00:40 +0600

    fixing done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import brnshap from "../assets/img/banner-shap.png"
 import Token from './Token'
+import PageMarquee from './PageMarquee'
 export default function Banner() {
   return (
     <div className='banner'>
       <div className="container">
         <div className="banner-content">
-          <h1>UNPEEL THE FUTURE: <span>IT'S BLUE!</span></h1>
+          <h1>UNPEEL THE FUTURE: <span>IT'S BLUE BANANA!</span></h1>
           <p>This is a rare blue banana. Hodl and you’ll become a bananillionaire. Ignore, and you’ll fail forever.</p>
           <Token />
         </div>
       </div>
-      <img className='brnshap' src={brnshap} alt="" />
+      <PageMarquee />
     </div>
   )
 }
--- src/components/PageMarquee.jsx
+++ src/components/PageMarquee.jsx
@@ -1,13 +1,18 @@
 import Marquee from "react-fast-marquee"
 import marqueeimg from "../assets/img/marqueeimg.png"
+import brnshap from "../assets/img/banner-shap.png"
 export default function PageMarquee() {
     return (
-        <div className="common-marquee">
-            <Marquee autoFill={true} speed={60}>
-                <div className="single-item">
-                    <img src={marqueeimg} alt="" />
-                </div>
-            </Marquee>
+        <div className="marquee-area">
+            <img className='brnshap' src={brnshap} alt="" />
+            <div className="common-marquee">
+                <Marquee autoFill={true} speed={60}>
+                    <div className="single-item">
+                        <img src={marqueeimg} alt="" />
+                    </div>
+                </Marquee>
+            </div>
+
         </div>
     )
 }
