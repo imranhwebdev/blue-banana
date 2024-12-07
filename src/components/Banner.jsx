8d88e536413558218357e8a74ed64ce352ea8da7
imran <imranh.coder@gmail.com>
2024-12-07 18:01:08 +0600

    fixing working

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Token from './Token'
 import PageMarquee from './PageMarquee'
+import banana from "../assets/img/banana.png"
 export default function Banner() {
   return (
     <div className='banner'>
@@ -9,6 +10,7 @@ export default function Banner() {
           <h1>UNPEEL THE FUTURE: <span>IT'S BLUE BANANA!</span></h1>
           <p>This is a rare blue banana. Hodl and you’ll become a bananillionaire. Ignore, and you’ll fail forever.</p>
           <Token />
+          <img src={banana} alt="" />
         </div>
       </div>
       <PageMarquee />
